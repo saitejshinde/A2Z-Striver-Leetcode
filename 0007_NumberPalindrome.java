67c9eee302261e1eaf8fc3a1c49d9cb1b5943971
saitejshinde <saitej.g.shinde@gmail.com>
2025-10-12 23:26:42 +0530

    Number Palidrome check To find that number follows the palindrome or not

---
+++ 0007_NumberPalindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class NumberPalindrome{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        boolean b = isPalindrome(a);
+
+        System.out.print(b);
+    }
+
+     public static boolean isPalindrome(int x) {
+        
+        if (x < 0) return false;
+
+        int original = x;
+        long reversed = 0; 
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+}
