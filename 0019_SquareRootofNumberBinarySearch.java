66b0adc1dc15c4f3c4e0bcb58b02677a3cae2e2f
saitejshinde <saitej.g.shinde@gmail.com>
2025-11-06 20:49:47 +0530

    0019 Square root using the Binary search approach

---
+++ 0019_SquareRootofNumberBinarySearch.java
@@ -0,0 +1,57 @@
+
+import java.util.Scanner;
+
+ class SquareRootofNumberBinarySearch {
+    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = 1;
+        while(a==1){
+
+            System.out.println("Want to continue");
+            a = sc.nextInt();
+            System.out.println("Enter the Number ");
+
+            int b = sc.nextInt();
+
+            System.out.println(mySqrt(b));
+
+        }
+    }
+
+    public static int mySqrt(int x) {
+
+
+        // int a = 0; 
+        // while(a * a < x){
+        //     if((a+1)*(a+1)>x){
+        //         break;
+        //     }
+        //     a++;
+        // }
+
+        if(x==0 || x==1){
+            return x;
+        }
+        int ans =0;
+        int l=1 , r= x/2; 
+        while(l<=r){
+            int mid = l+ (r-l)/2;
+
+            if((long)mid * mid == x){
+                ans= mid;
+                break;
+            }
+            else if((long)mid * mid < x){
+                ans = mid;
+                l=mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+
+        }
+        return ans;
+        // return (int)Math.round(Math.exp(0.5*Math.log(x))) ;
+    }
+}
